3aef0bce1672bda40299819e87c67746d5a0e2b6
erperreault <69309175+erperreault@users.noreply.github.com>
2021-03-29 19:59:58 -0400

    Add files via upload

---
+++ chapter 1/10_replaceWhiteSpace.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/* reduce all multi-spaces to one space */
+/* test characters here: \ \    \       */
+int main() {
+    int c;
+    
+    while ((c = getchar()) != EOF) {
+        if (c == '\t') {
+            printf("\\\t");
+        } else if (c == '\b') {
+            printf("\\\b");
+        } else if (c == '\\') {
+            printf("\\\\");
+        } else {
+            putchar(c);
+        }
+    }
+}
