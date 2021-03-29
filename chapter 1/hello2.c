ce533da0e3b25159ac136a580afd88129a6eb3ca
erperreault <69309175+erperreault@users.noreply.github.com>
2021-03-29 18:25:47 -0400

    Add files via upload

---
+++ chapter 1/hello2.c
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+int main() { 
+    printf("hello, world\n"); 
+    printf("hello, world\n")
+    printf("hello, world\n";
+    printf("hello, world\n);
+    printf("hello, world\");
+    printf("hello, worldn");
+    printf(hello, world\n");    
+    printf"hello, world\n");  
+    print("hello, world\n"); 
+    printf("hello, world\n"); 
+}
